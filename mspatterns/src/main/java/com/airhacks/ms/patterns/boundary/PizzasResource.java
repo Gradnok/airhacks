b89abe2c3909d483bf0327db1fd3a00646ff24eb
Adam Bien <abien@adam-bien.com>
2017-03-21 16:41:55 +0100

    bulkheads implemented

--- mspatterns/src/main/java/com/airhacks/ms/patterns/boundary/PizzasResource.java
+++ mspatterns/src/main/java/com/airhacks/ms/patterns/boundary/PizzasResource.java
@@ -1,23 +1,37 @@
 
 package com.airhacks.ms.patterns.boundary;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
-/**
- *
- * @author airhacks.com
- */
+@Stateless
 @Path("pizzas")
 public class PizzasResource {
 
     @Inject
     PizzaCatalog catalog;
 
+    @Resource
+    ManagedExecutorService mes;
+
     @GET
-    public String all() {
-        return catalog.all();
+    public void all(@Suspended AsyncResponse response) {
+        response.setTimeout(2, TimeUnit.SECONDS);
+        supplyAsync(this.catalog::all, mes).
+                thenApply(this::decorate).
+                thenAccept(response::resume);
+    }
+
+    String decorate(String input) {
+        return "+++ " + input + "++++";
     }
 
 
