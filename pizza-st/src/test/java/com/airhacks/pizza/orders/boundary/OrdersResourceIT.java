95eb34f95a0e498d3cf1ebaecce2d4d76329790a
Adam Bien <abien@adam-bien.com>
2017-03-21 10:29:49 +0100

    system test implemented

---
+++ pizza-st/src/test/java/com/airhacks/pizza/orders/boundary/OrdersResourceIT.java
@@ -0,0 +1,39 @@
+/*
+ */
+package com.airhacks.pizza.orders.boundary;
+
+import javax.json.JsonArray;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class OrdersResourceIT {
+
+    private Client client;
+    private WebTarget tut;
+
+    @Before
+    public void initClient() {
+        this.client = ClientBuilder.newClient();
+        this.tut = this.client.target("http://localhost:8080/pizza/resources/orders");
+    }
+
+    @Test
+    public void orders() {
+        Response response = this.tut.request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(), is(200));
+        JsonArray ordersAsJson = response.readEntity(JsonArray.class);
+        System.out.println("ordersAsJson = " + ordersAsJson);
+    }
+
+}
