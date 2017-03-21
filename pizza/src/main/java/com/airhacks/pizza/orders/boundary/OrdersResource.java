bc0461debc27a13dccca952a74d19deee66e6465
Adam Bien <abien@adam-bien.com>
2017-03-21 10:09:28 +0100

    initial creation of pizza orders service

---
+++ pizza/src/main/java/com/airhacks/pizza/orders/boundary/OrdersResource.java
@@ -0,0 +1,25 @@
+
+package com.airhacks.pizza.orders.boundary;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonReader;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Path("orders")
+public class OrdersResource {
+
+    @GET
+    public JsonArray all() {
+        try (JsonReader jsonReader = Json.createReader(this.getClass().getResourceAsStream("/orders.json"))) {
+            return jsonReader.readArray();
+        }
+    }
+
+
+}
